98dbf557e253478bb2430d2da2603c2e7564f773
Vanessa Maria <vanessa.silva5205@gmail.com>
2023-10-08 20:15:58 -0300

    atualização da página principal

--- src/main/java/br/edu/centroeducacional/controller/IndexController.java
+++ src/main/java/br/edu/centroeducacional/controller/IndexController.java
@@ -3,6 +3,7 @@ package br.edu.centroeducacional.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,6 +13,7 @@ public class IndexController {
 	@GetMapping("/")
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("/index");
+		modelAndView.addObject("painel", "ativo");
 		return modelAndView;
 	}
 
