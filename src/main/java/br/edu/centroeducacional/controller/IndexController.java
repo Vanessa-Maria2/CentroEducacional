c100af2067472b75683223a290505a97ec534969
Vanessa <vanessa.maria@imd.ufrn.br>
2024-02-17 23:08:09 -0300

    Adicionada página de login

--- src/main/java/br/edu/centroeducacional/controller/IndexController.java
+++ src/main/java/br/edu/centroeducacional/controller/IndexController.java
@@ -3,7 +3,6 @@ package br.edu.centroeducacional.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +13,12 @@ public class IndexController {
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("/index");
 		modelAndView.addObject("painel", "ativo");
+		return modelAndView;
+	}
+
+	@GetMapping("/login")
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView("/login");
 		return modelAndView;
 	}
 
